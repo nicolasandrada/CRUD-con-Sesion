f8aee59bb79e30a60bc19392d4f40c766f8b8953
valen <nicolas.andrada@hotmail.com>
2022-09-14 14:39:54 -0300

    login y registro

--- auth/register.php
+++ auth/register.php
@@ -13,7 +13,10 @@ $photo = 'imagen.png';
 $sql = "SELECT * FROM user WHERE name = '$name'";
 $r = $con->query($sql);
 
+//cuenta el numero de resultados devueltos
 if($r->num_rows > 0){
+
+    //reenvia hacia otra pag
     header("location: ../index.php?error=ya existe el usuario");
 }else{
     $sql = "INSERT INTO user VALUES ( null, '$name', '$pass', '$birth_date', '$sex', '$photo')";
