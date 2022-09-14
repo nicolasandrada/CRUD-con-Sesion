361670ae695f25f89fafc7b1e371e57ca9089f05
valen <nicolas.andrada@hotmail.com>
2022-09-14 15:03:08 -0300

    login

--- auth/login.php
+++ auth/login.php
@@ -1,4 +1,5 @@
 <?php
+//utilizar sesiones
 session_start();
 
 include '../db/conexion.php'; 
@@ -6,11 +7,13 @@ include '../db/conexion.php';
 $name = $_POST['name'];
 $pass = md5($_POST['pass']) ;
 
+//selecciona el usario que coincide con el nombre de usuario y contraseña
 $sql = "SELECT * FROM user WHERE name = '$name' AND pass = '$pass' ";
 
 $r = $con->query($sql);
 
+//si coincide, guarda una sesion con los datos del usuario
 if($r->num_rows > 0 ){
     $_SESSION['user'] = $r->fetch_assoc();
-    var_dump($_SESSION['user']);
+    //var_dump($_SESSION['user']['name']);
 }
